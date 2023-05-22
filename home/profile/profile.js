de5d48f12e8f513a7c3c0e71e2602c9d5c9e0ab8
JesusGarciaPixa <123981276+JesusGarciaPixa@users.noreply.github.com>
2023-05-21 21:29:34 -0700

    update password

--- home/profile/profile.js
+++ home/profile/profile.js
@@ -1,4 +1,10 @@
 import { db } from "../../firebase.js";
+import {
+  getAuth,
+  updatePassword,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+
+import { auth } from "../../firebase.js";
 
 import {
   collection,
@@ -9,6 +15,9 @@ import {
 
 const nickInput = document.getElementById("nick");
 const updateButton = document.getElementById("update");
+const passwordInput = document.getElementById("password");
+const updatePasswordButton = document.getElementById("updatePassword");
+
 let idRef = null;
 
 const loadNick = async () => {
@@ -32,16 +41,26 @@ const updateNick = async () => {
   });
 };
 
-updateButton.addEventListener("click", updateNick);
-
-loadNick();
-
 const checkIfValidUser = () => {
   if (!localStorage.getItem("uid")) {
     window.location.href = "http://localhost:5500/index.html";
   }
 };
 
-//carga inicial
+const changePassword = async () => {
+  const { currentUser } = auth;
+  try {
+    await updatePassword(currentUser, passwordInput.value);
+    passwordInput.value = "";
+    alert("contraseña actualizada con exito");
+  } catch (error) {
+    console.log(error);
+    alert("ocurrio un error al actualizar contraseña");
+  }
+};
 
+//carga inicial
 checkIfValidUser();
+loadNick();
+updateButton.addEventListener("click", updateNick);
+updatePasswordButton.addEventListener("click", changePassword);
